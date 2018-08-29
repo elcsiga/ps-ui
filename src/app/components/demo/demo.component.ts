45e7749b16df8f40ccd8a98bdbff0387ac6c7dad
Andras Kovacs <Andras.Kovacs4@ge.com>
2018-08-29 16:12:41 +0200

    first try

--- src/app/components/demo/demo.component.ts
+++ src/app/components/demo/demo.component.ts
@@ -2,46 +2,51 @@ import {Component, OnInit} from '@angular/core';
 import {Apollo, gql} from 'apollo-angular-boost';
 
 interface DemoData {
-  test: any;
+    test: any;
 }
 
 @Component({
-  selector: 'app-demo',
-  template: `
-    <div *ngIf="loading">
-      Loading...
-    </div>
-    <div *ngIf="errors">
-      Error :(
-    </div>
-    <div *ngIf="data">
-      {{ data | json }}
-    </div>
-  `,
+    selector: 'app-demo',
+    template: `
+        <div *ngIf="loading">
+            <h3>Loading</h3>
+        </div>
+        <div *ngIf="error">
+            <h3>Error</h3> {{ error | json }}
+        </div>
+        <div *ngIf="data">
+            <h3>Success</h3> {{ data | json }}
+        </div>
+    `,
 })
 export class DemoComponent implements OnInit {
-  data: DemoData;
-  loading: boolean;
-  errors: any[];
+    data: DemoData;
+    loading: boolean;
+    error: any;
 
-  constructor(private apollo: Apollo) {}
+    constructor(private apollo: Apollo) {
+    }
 
-  ngOnInit() {
-    this.apollo
-      .watchQuery<DemoData>({
-        query: gql`
-          {
-            rates(currency: "USD") {
-              currency
-              rate
+    ngOnInit() {
+        this.apollo
+            .watchQuery<DemoData>({
+                query: gql`
+                {
+                    blogposts(limit: 10) {
+                      title
+                      thumbnail
+                      content
+                    }
+                }
+                `,
+            })
+            .valueChanges.subscribe(result => {
+                this.data = result.data;
+                this.loading = result.loading;
+                this.error = result.errors ? { graphQLErrors: result.errors } : null;
+            }, error => {
+                this.error = error;
             }
-          }
-        `,
-      })
-      .valueChanges.subscribe(result => {
-      this.data = result.data;
-      this.loading = result.loading;
-      this.errors = result.errors;
-    });
-  }
+        );
+    }
 }
